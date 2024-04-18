b08a39b6d7060d6e0875ee381fb150f1cb262c38
tsaolun <ycl18@live.com>
2024-04-18 10:00:39 +0800

    modified:   main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,12 @@ import { serveFile } from "https://deno.land/std@0.177.0/http/file_server.ts"
 
 serve((req: Request) => {
     const url = new URL(req.url);
-    // if (url.pathname.startsWith("/img")) {
-    //     return serveFile(req, "./demo"+ url.pathname)
-    // }
     console.log(url.pathname)
-    if (url.pathname.startsWith("/x_files")) {
-        return serveFile(req, "./"+ url.pathname)
+    if (url.pathname === "/") {
+        return serveFile(req, "./home.html")
     }
-    return serveFile(req, "./home.html")
+    if (url.pathname == "/declaration") {
+        return serveFile(req, "./declaration.html")
+    }
+    return serveFile(req, "."+ url.pathname)
 });
